348e8c829ddc5ec4a51aae8016c9179e0c148f27
Sven Glauser <svenglauser04@gmail.com>
2026-09-02 17:50:19 +0200

    - Suppression des config inutiles

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -33,7 +33,6 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({eventCoalescing: true}),
     provideRouter(routes),
     provideHttpClient(
-      withFetch(),
       withInterceptors([
         includeBearerTokenInterceptor,
         catchHttpExceptionInterceptor
